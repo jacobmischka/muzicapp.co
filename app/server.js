be2a60291b42fb4cb6450155085d56c1d8742731
Jacob Mischka <jacob@mischka.me>
2018-03-14 12:34:49 -0500

    Initial commit of new version

---
+++ app/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import compression from 'compression';
+import sapper from 'sapper';
+import serveStatic from 'serve-static';
+import fetch from 'node-fetch';
+import { routes } from './manifest/server.js';
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+// this allows us to do e.g. `fetch('/api/blog')` on the server
+global.fetch = (url, opts) => {
+	if (url[0] === '/') url = `http://localhost:${PORT}${url}`;
+	return fetch(url, opts);
+};
+
+app.use(compression({ threshold: 0 }));
+
+app.use(serveStatic('assets'));
+
+app.use(sapper({
+	routes
+}));
+
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
